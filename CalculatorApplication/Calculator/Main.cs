d5b7d712b024ca3e7a0b3c29b7181b74a3770b74
colehoward-nz <74393866+colehoward-nz@users.noreply.github.com>
2020-12-10 17:23:07 +1300

    Initial commit

---
+++ CalculatorApplication/Calculator/Main.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace CalculatorApplication.Calculator
+{
+    public class Main
+    {
+        string[] operationList = new string[4] { "+", "-", "*", "/" };
+
+        string operation;
+        int firstNum;
+        int secondNum;
+        int answer;
+        public void RunStuff()
+        {
+            Console.WriteLine("What type of calculation do you want to perform? (+, -, * or /)");
+            operation = Console.ReadLine();
+            if (operationList.Contains(operation))
+            {
+                Console.WriteLine("Enter the first number:");
+                while (Int32.TryParse(Console.ReadLine(), out int result) == false)
+                {
+                    Console.WriteLine("That isn't a number!");
+                    
+                }
+                firstNum = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the second number:");
+                while (Int32.TryParse(Console.ReadLine(), out int result) == false)
+                {
+                    Console.WriteLine("That isn't a number!");
+                    
+                }
+                secondNum = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("You've selected " + firstNum + " and " + secondNum);
+                if (operation == "+")
+                {
+                    answer = firstNum + secondNum;
+                }
+                else if (operation == "-")
+                {
+                    answer = firstNum - secondNum;
+                }
+                else if (operation == "*")
+                {
+                    answer = firstNum * secondNum;
+                }
+                else if (operation == "/")
+                {
+                    answer = firstNum / secondNum;
+                }
+                Console.WriteLine();
+                Console.WriteLine(firstNum + " " + operation + " " + secondNum);
+                Console.WriteLine("= " + answer);
+                Console.WriteLine();
+                
+            }
+            else
+            {
+                RunStuff();
+            }
+        }
+    }
+}
---
+++ CalculatorApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CalculatorApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calculator.Main mainMethod = new Calculator.Main();
+            
+            bool continueProgram = true;
+
+            while (continueProgram == true)
+            {
+                mainMethod.RunStuff();
+
+                Console.WriteLine("Would you like to make another calculation?");
+                Console.WriteLine("   Enter either: true or false");
+                continueProgram = bool.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Thank you for using the program!");
+            Console.WriteLine();
+        }
+    }
+}
